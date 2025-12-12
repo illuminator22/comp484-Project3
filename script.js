cf1e0b97b47779784cd2e1faefd660c1b17f2cfb
Ivan <109504040+illuminator22@users.noreply.github.com>
2025-12-11 19:37:28 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ console.log("Status Manager Started");
 // global var for the timer
 let intervalId = null;
 
-// grab all the elements we need
+// grabbing all the elements we need
 const mainTitle = document.querySelector("#main-title");
 const toggleButton = document.getElementById("toggle-button");
 const statusOutput = document.querySelector("#status-output");
 const timerButton = document.getElementById("timer-button");
 const controlPanel = document.getElementById("control-panel");
 
-// Task 3 - change the title text
+// Task 3 - changing the title text
 mainTitle.innerHTML = "DOM Project: Ready!";
 
-// Task 4 - add data attribute to toggle button
+// Task 4 - adding data attribute to toggle button
 toggleButton.setAttribute("data-action", "status-toggle");
 
 // Task 9 - make all list items blue
@@ -26,7 +26,7 @@ function highlightListItems() {
 }
 highlightListItems();
 
-// Task 8 - add a timestamp to the status div
+// Task 8 - adding a timestamp to the status div
 function createTimestamp() {
     const span = document.createElement("span");
     span.innerHTML = new Date().toLocaleTimeString();
@@ -34,7 +34,7 @@ function createTimestamp() {
     statusOutput.appendChild(document.createElement("br"));
 }
 
-// Tasks 5, 6, 7 - toggle the status box on click
+// Tasks 5, 6, 7 - toggling the status box on click
 function toggleStatus(e) {
     e.preventDefault(); // stop the link from jumping
     statusOutput.classList.toggle("hidden");
